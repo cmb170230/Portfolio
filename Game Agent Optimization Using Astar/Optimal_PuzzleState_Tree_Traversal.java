1aa80fe3d5dc874506bc337dc25201847bc3d045
cmb170230 <91343314+cmb170230@users.noreply.github.com>
2023-03-16 15:24:01 -0500

    Update Optimal_PuzzleState_Tree_Traversal.java

--- Game Agent Optimization Using Astar/Optimal_PuzzleState_Tree_Traversal.java
+++ Game Agent Optimization Using Astar/Optimal_PuzzleState_Tree_Traversal.java
@@ -1,8 +1,11 @@
 package hw_one;
 import java.io.*;
 import java.util.*;
+
 //empty spot denoted by -1
 //filepath on my machine: C:\Users\benne\Google Drive\Laptop Sync\UTD\Old\Fall 2022\CS 4365\Homeworks\HW1\src\input.txt
+
+
 class Optimal_PuzzleState_Tree_Traversal {
 	public static void main(String[] args) {
 		//set up file input
@@ -12,6 +15,8 @@ class Optimal_PuzzleState_Tree_Traversal {
 		System.out.println(filePath);
 		File inFile = new File(filePath);
 		Scanner inputFile;
+
+		//read in the starting state from a given input file- the empty space is represented by *, so when parseInt fails we know the space is supposed to be empty
 		try {
 			inputFile = new Scanner(inFile);
 			int[] input_init = new int[9];
@@ -23,49 +28,112 @@ class Optimal_PuzzleState_Tree_Traversal {
 					input_init[i] = -1;
 				}
 			}
-			int[] goal = {7,8,1,6,-1,2,5,4,3};
+
+			//initialize requisite variables
+			final int[] goal = {7,8,1,6,-1,2,5,4,3};
 			Node initialNode = new Node(input_init);
 			Node goalNode = new Node(goal);
 			int max_depth = 10;
 			
+			//prompt the user for their desired algorithm
 			System.out.println("Please enter the algorithm:\n1- depth-first search\n2- iterative deepening\n3- A* algorithm");
 			int algo = userIn.nextInt();
-			//System.out.println(algo);
 			
+			//the most basic depth first search tree implementation- constructs the tree, then searches it up to a maximum depth and prints the resulting path to the goal
 			if(algo == 1) {
 				try {
-				DFStree test1 = new DFStree(initialNode);
-				test1.buildDFSTree(goalNode, max_depth);
-				LinkedList<Node> test1l = test1.dfs(goalNode, max_depth);
-				printBackList(test1l);
-				System.out.println("States Enqueued:" + test1.stateEnq);
+					DFStree test1 = new DFStree(initialNode);
+					test1.buildDFSTree(goalNode, max_depth);
+					LinkedList<Node> test1l = test1.dfs(goalNode, max_depth);
+					printBackList(test1l);
+					System.out.println("States Enqueued:" + test1.stateEnq);
 				}
 				catch(NullPointerException e) {
 					System.out.println("No goal state found");
 				}
 			}
+			//repeatedly calls DFS starting with a restricted depth and gradually increases it until the goal is found
 			else if(algo == 2) {
 				try {
-				int limit = 0;
-				int enq = 0;
-				LinkedList <Node> test2l = null;
-				do {
-					DFStree test2 = new DFStree(initialNode);
-					limit++;
-					test2.buildDFSTree(goalNode, limit);
-					test2l = test2.dfs(goalNode, limit);
-					enq += test2.stateEnq;
-				}while(test2l == null && limit < max_depth);
-				printBackList(test2l);
-				System.out.println("States Enqueued:" + enq);
+					//initialize limit, count of enqueued states, and list of path to goal
+					int limit = 0;
+					int enq = 0;
+					LinkedList <Node> test2l = null;
+
+					//while the goal is not found and the maximum depth has not been reached, build and search a DFStree, incrementing the depth limit by 1 each time
+					do {
+						DFStree test2 = new DFStree(initialNode);
+						limit++;
+						test2.buildDFSTree(goalNode, limit);
+						test2l = test2.dfs(goalNode, limit);
+						enq += test2.stateEnq;
+					}while(test2l == null && limit < max_depth);
+
+					printBackList(test2l);
+					System.out.println("States Enqueued:" + enq);
 				}
+				//if depth limit is hit before goal is found, printbacklist will throw a nullpointerexception, indicating lack of path to goal
 				catch(NullPointerException e) {
 					System.out.println("No Goal State Found");
 				}
 			}
+			//Implementation of A* Search Algorithm
 			else if(algo == 3) {
-				System.out.println("Please select hueristic 1 or 2 by typing 1 or 2.");
+				//prompt user for hueristic selection
+				System.out.println("Please select hueristic 1 (Manhattan Distance) or 2 (# of Misplaced) by typing 1 or 2.");
 				int huer = userIn.nextInt();
+
+				//if an invalid value is selected for the hueristic, indicate and exit
+				if(huer != 1 && huer != 2){
+					System.out.println("Please enter a valid value for hueristic selection.");
+					userIn.close();
+					return;
+				}
+
+				//initialize all necessary variables for A* search
+				int counter = 0;
+				AstarNode astInit = new AstarNode(initialNode, AstarNode.calcDist(initialNode, goalNode, huer));
+				PriorityQueue<AstarNode> frontier = new PriorityQueue<>();
+				Stack<AstarNode> expanded = new Stack<>();
+
+				//A*'s efficency comes from the use of the distance metric to try to only expand (i.e. generate children for) the nodes 'closest' to the goal
+				//start by adding the initial AstarNode to the 'frontier' (Priority Queue, the reason why AstarNode implements comparable)
+				frontier.add(astInit);
+
+				//the 'distance' between the goal and itself is zero by definition, so while the goal state is not enqueued (when it is, it will automatically be first in queue)
+				while(frontier.peek().distance != 0) {
+					//pop the closest node to the goal off the frontier, push it to the stack, and expand it
+					AstarNode temp = frontier.remove();
+					expanded.push(temp);
+					AstarNode[] tempch = AstarNode.aChildrenGen(temp, goalNode, huer);
+					//for all of the possible children
+					for(int i = 0; i < 4; i++)
+						//if the child exists, add it to the frontier
+						if(tempch[i] != null) {
+							frontier.add(tempch[i]);
+							//the stack allows for the tracking of the path itself
+							//iterate through the stack, if a given state in the stack reappears at the front of the frontier, there must be a shorter path through that state
+							//therefore pop the stack until everything at that point is removed
+							for(int j = 0; j < expanded.size() ; j++)
+								if(!expanded.elementAt(j).node.isDifferentState(frontier.peek().node))
+									for(int k = 0; k < j; k++)
+										expanded.pop();								
+						}
+					counter++;
+				}
+
+				//finally, grab the enqueued goal state and push it to the stack
+				expanded.push(frontier.remove());
+				LinkedList<Node> printList = new LinkedList<>();
+				while(expanded.peek().node.isDifferentState(initialNode)) {
+					printList.add(expanded.pop().node);
+				}
+				printList.add(expanded.pop().node);
+				printBackList(printList);
+
+				System.out.println("States Enqueued:" + counter);
+
+				/*Old Version using separate function for the two different hueristics
 				//Manhattan Distance
 				if(huer == 1) {
 					int counter = 0;
@@ -129,7 +197,7 @@ class Optimal_PuzzleState_Tree_Traversal {
 					System.out.println("States Enqueued: " + counter);
 				}
 				else
-					System.out.println("Not a valid hueristic.");
+					System.out.println("Not a valid hueristic.");*/
 			}
 			else
 				System.out.println("Error: no algorithm matching that input.");
@@ -139,7 +207,8 @@ class Optimal_PuzzleState_Tree_Traversal {
 		userIn.close();
 	}
 	
-	public static void printBackList(LinkedList<Node> list) {
+	//old method before realizing that the library implementation of the linked list is doubly linked
+	/*public static void printBackList(LinkedList<Node> list) {
 		int len = DFStree.listLength(list);
 		for(int i = len; i > 0; i--) {
 			ListIterator<Node> liter = list.listIterator();
@@ -148,19 +217,33 @@ class Optimal_PuzzleState_Tree_Traversal {
 			liter.next().printNode();
 			System.out.println();
 		}
+	}*/
+
+	//new method taking advantage of doubly linked nature of the linkedlist class
+	public static void printBackList(LinkedList<Node> list){
+		ListIterator<Node> listIter = list.listIterator();
+		while(listIter.hasNext())
+			listIter.next();
+		while(listIter.hasPrevious()){
+			listIter.previous().printNode();
+			System.out.println();
+		}
 	}
 	
 }
 
-class AstarNode implements Comparable<AstarNode>{
+//specialized class for use with the A* algorithm- wrapper for node adding a distance metric
+class AstarNode extends Node implements Comparable<AstarNode>{
 	public int distance;
 	public Node node;
 	
+	//constructor for a node designed for use with the A* algorithm, simply takes in a node and associates a notion of distance from the goal with it
 	public AstarNode(Node n, int d) {
 		node = n;
 		distance = d;
-		}
+	}
 	
+	//definition of comparison method for the comparable interface
 	public int compareTo(AstarNode n2) {
 		if (this.distance == n2.distance)
 			return 0;
@@ -169,7 +252,16 @@ class AstarNode implements Comparable<AstarNode>{
 		else
 			return -1;
 	}
+
+	//addition to allow for two different A* Child Generator methods to be merged
+	public static int calcDist(Node n, Node goal, int huer) {
+		if(huer == 1)
+			return manndist(n, goal);
+		else
+			return misplacedist(n, goal);
+	}
 	
+	//for each position of a value in the current state, find where the value is supposed to be in the goal and calculate the difference; return the sum of all said differences
 	public static int manndist(Node n, Node goal) {
 		int dist = 0;
 		for(int i = 0; i < 3; i++)
@@ -185,6 +277,7 @@ class AstarNode implements Comparable<AstarNode>{
 		return dist;
 	}
 	
+	//for each position of the value in the current node, just determine whether it is misplaced or not and return the number of misplaced nodes
 	public static int misplacedist(Node n, Node goal) {
 		int dist = 0;
 		for(int i = 0; i < 3; i++)
@@ -193,14 +286,19 @@ class AstarNode implements Comparable<AstarNode>{
 					dist++;
 		return dist;
 	}
-	
+
+	//generate children with distances calculated with hueristic 1: Manhattan Distance
 	public static AstarNode[] a1ChildrenGen(AstarNode node, Node goal){
+		//leverage the logic of the children generation in the DFStree class to do the majority of the work
 		DFStree temp = new DFStree();
 		Node[] nodeChildren = temp.childrenGen(node.node);
+
+		/* Old, late-night version of below for loop
 		AstarNode child1 = null;
 		AstarNode child2 = null;
 		AstarNode child3 = null;
 		AstarNode child4 = null;
+
 		if(nodeChildren[0] != null)
 			child1 = new AstarNode(nodeChildren[0], manndist(nodeChildren[0], goal));
 		if(nodeChildren[1] != null)
@@ -213,12 +311,22 @@ class AstarNode implements Comparable<AstarNode>{
 		ret[0] = child1;
 		ret[1] = child2;
 		ret[2] = child3;
-		ret[3] = child4;
-		return ret;
+		ret[3] = child4;*/
+
+		//convert children returned from DFS children gen to AstarNodes and return array of converted nodes
+		AstarNode[] retArr = {null, null, null, null};
+		for(int i = 0; i < 4; i++){
+			if(nodeChildren[i] != null)
+			retArr[i] = new AstarNode(nodeChildren[i], manndist(nodeChildren[i], goal));
+		}
+		return retArr;
 	}
+
+	//generate children with distances calculated with hueristic 2: Misplaced Values
 	public static AstarNode[] a2ChildrenGen(AstarNode node, Node goal){
 		DFStree temp = new DFStree();
 		Node[] nodeChildren = temp.childrenGen(node.node);
+		/*Old, late-night version of below for loop
 		AstarNode child1 = null;
 		AstarNode child2 = null;
 		AstarNode child3 = null;
@@ -235,11 +343,31 @@ class AstarNode implements Comparable<AstarNode>{
 		ret[0] = child1;
 		ret[1] = child2;
 		ret[2] = child3;
-		ret[3] = child4;
-		return ret;
+		ret[3] = child4;*/
+
+		AstarNode[] retArr = {null, null, null, null};
+		for(int i = 0; i < 4; i++){
+			if(nodeChildren[i] != null)
+			retArr[i] = new AstarNode(nodeChildren[i], misplacedist(nodeChildren[i], goal));
+		}
+		return retArr;
+	}
+
+	//added method to clean up how children are generated in A* approach
+	public static AstarNode[] aChildrenGen(AstarNode node, Node goal, int huer){
+		DFStree temp = new DFStree();
+		Node[] nodeChildren = temp.childrenGen(node.node);
+
+		AstarNode[] retArr = {null, null, null, null};
+		for(int i = 0; i < 4; i++){
+			if(nodeChildren[i] != null)
+			retArr[i] = new AstarNode(nodeChildren[i], calcDist(nodeChildren[i], goal, huer));
+		}
+		return retArr;
 	}
 }
 
+//class for implementation of both raw depth-first and iterative deepening searches
 class DFStree{
 	private Node head;
 	private int buildNumber = 0;
@@ -253,10 +381,13 @@ class DFStree{
 		return dfsRecur(this.head, goal, maxDepth);
 	}
 	
+	//method for actually performing the depth-first search
 	private LinkedList<Node> dfsRecur(Node node, Node goal, int curHeight){
+		//if a leaf is found or the limit of the depth of the search is met, return without a path from the head to the goal
 		if(node == null || curHeight == 0)
 			return null;
 		else {
+			//if the goal is found along a given path, begin constructing the return list 
 			if(!node.isDifferentState(goal)) {
 				stateEnq++;
 				LinkedList<Node> ret = new LinkedList<>();
@@ -264,12 +395,16 @@ class DFStree{
 				return ret;
 			}
 			else {
+				//each time this function is called, increment the number of states enqueued
 				stateEnq++;
+
+				//keep recursing down a node's given children if possible
 				LinkedList<Node> list1 = dfsRecur(node.child1, goal, curHeight-1);
 				LinkedList<Node> list2 = dfsRecur(node.child2, goal, curHeight-1);
 				LinkedList<Node> list3 = dfsRecur(node.child3, goal, curHeight-1);
 				LinkedList<Node> list4 = dfsRecur(node.child4, goal, curHeight-1);
 				
+				//on the return cascade, if a list was returned from the children, append the current node to it
 				if(list1 != null)
 					list1.add(node);
 				if(list2 != null)
@@ -279,11 +414,13 @@ class DFStree{
 				if(list4 != null)
 					list4.add(node);
 				
+				//calculate the size of the lists, for comparison's sake if the list is null then have it be maximum value
 				int link1size = listLength(list1);
 				int link2size = listLength(list2);
 				int link3size = listLength(list3);
 				int link4size = listLength(list4);
 				
+				//the shortest path to the goal is the best, so find the length of the shortest list and return it (if all lists are null, then return null)
 				int minSize = Math.min(link1size, Math.min(link2size, Math.min(link3size, link4size)));
 				if(minSize == link1size && minSize != Integer.MAX_VALUE)
 					return list1;
@@ -299,7 +436,7 @@ class DFStree{
 		}
 	}
 	
-	
+	//helper method to find the length of a list with added behavior for null lists for use in comparison above
 	public static int listLength(LinkedList<Node> list) {
 		if(list == null)
 			return Integer.MAX_VALUE;
@@ -320,11 +457,15 @@ class DFStree{
 		recursiveBuild(head, goal_node, max_depth, null);
 	}
 	
-	private void recursiveBuild(Node node, Node stop, int depth, Node parentn) {
+	
+	private void recursiveBuild(Node node, Node stop, int height, Node parentn) {
+		//properly assign the parent attribute, then check to see if the goal has been found
 		node.parent = parentn;
 		boolean diffFlag = node.isDifferentState(stop);
 				
-		if((depth >= 0) && diffFlag /*&& !initFlag*/) {
+		//if the maximum depth has not been exceeded and the goal has not been found
+		if((height >= 0) && diffFlag) {
+			//increment buildNumber (tracks how many times children are built) and generate children, assigning them to the node currently being operated on
 			buildNumber++;
 			Node[] children = childrenGen(node);
 			
@@ -333,21 +474,25 @@ class DFStree{
 			node.child3 = children[2];
 			node.child4 = children[3];
 			
-			node.setDepth(depth);
-			--depth;
+			//somewhere along the way height was decided to be a better metric for this implementation, so set the height of the current node and decrement
+			node.setHeight(height);
+			--height;
 			
+			//for each of the children, if they were generated in the childrenGen method call and they do not cause a cycle, 
 			if(node.child1 != null && checkParents(node.child1, node))
-				recursiveBuild(node.child1, stop, depth, node);
+				recursiveBuild(node.child1, stop, height, node);
 			if(node.child2 != null && checkParents(node.child2, node))
-				recursiveBuild(node.child2, stop, depth, node);
+				recursiveBuild(node.child2, stop, height, node);
 			if(node.child3 != null && checkParents(node.child3, node))
-				recursiveBuild(node.child3, stop, depth, node);
+				recursiveBuild(node.child3, stop, height, node);
 			if(node.child4 != null && checkParents(node.child4, node)) 
-				recursiveBuild(node.child4, stop, depth, node);
+				recursiveBuild(node.child4, stop, height, node);
 		}
 	}
 	
+	//from a given node, generates all possible children (i.e. next game states from this point) allowable by the rules of the game
 	public Node[] childrenGen(Node n) {
+		//initialize necessary varaibles
 		Node child_one = null, child_two = null, child_three = null, child_four = null;
 		int ind1 = -2, ind2 = -2;
 		//find index of empty space
@@ -359,6 +504,7 @@ class DFStree{
 				}
 			}
 		}
+
 		//construct possible moves (swaps) based on index location
 		//case 1: empty is swappable with position above
 		if(ind1 - 1 >= 0) {
@@ -381,12 +527,12 @@ class DFStree{
 			child_four = swap(child_four, ind1, ind2, ind1, ind2+1);
 		}
 		
-			
+		//once all possible children are constructed, return them as an array of Nodes
 		Node[] returnNode = {child_one, child_two, child_three, child_four};
-		
-	return returnNode;
+		return returnNode;
 	}
 	
+	//swapping method to aid in programmatic representation of allowable game rules
 	private Node swap(Node n, int i, int j, int ii, int jj) {
 		int temp = n.state[ii][jj];
 		n.state[ii][jj] = n.state[i][j];
@@ -395,33 +541,38 @@ class DFStree{
 		return n;
 	}
 	
+	//the below methods allow for the detection of cycles in children generation
 	private boolean checkParents(Node n, Node nodeParent) {
 		return checkParentRecur(n, nodeParent);
 	}
 	
+	//checks that a given node does not match any of its ancestors (thus creating a repeating pattern/cycle)
 	private boolean checkParentRecur(Node n, Node nodeParent) {
 		boolean diffFlag = n.isDifferentState(nodeParent);
 		boolean headFlag = nodeParent.isDifferentState(head);
+		//if node is still different and not at the head yet, keep going
 		if(diffFlag && headFlag)
 			return checkParentRecur(n, nodeParent.parent);
-		else if (!diffFlag && !headFlag)
+		//if the node is not different from an ancestor, return false
+		else if (!diffFlag /*&& headFlag*/)
 			return false;
+		//if the node is different from all ancestors and the head of the tree is reached, allow further children to be generated in the branch
 		else
 			return true;
 	}
 	
+	//allow (private) number of states enqueued to be printed out
 	public void dispBuildNo() {System.out.println(buildNumber);}
-	
-	
-	//public int getEnqueued() {return buildNumber;}
 }
 
 class Node{
+	//class attributes for a Node
 	int[][] state = new int[3][3];
-	int depth = 0;
+	int height = 0;
 	Node child1, child2, child3, child4;
 	Node parent;
 	
+	//default constructor, initialize everything to zero/null where relevant
 	public Node() {
 		for(int i = 0; i < 3; i++)
 			for(int j = 0; j < 3; j++)
@@ -433,6 +584,7 @@ class Node{
 		child4 = null;		
 	}
 	
+	//constructor to allow single dimensional array to be passed in as a state argument
 	public Node(int[] passState) {
 		int increment = 0;
 		for(int i = 0; i < 3; i++)
@@ -447,15 +599,17 @@ class Node{
 		child4 = null;	
 	}
 	
-	//deep copy
+	//constructor that initializes the new node to be a deep copy of the node passed in
 	public Node(Node copy) {
 		for(int i = 0; i < 3; i++)
 			for(int j = 0; j < 3; j++) 
 				this.state[i][j] = copy.state[i][j];
-		}
+	}
 	
-	public void setDepth(int d) {depth = d;}
+	//setter method to indicate the height of a given node
+	public void setHeight(int h) {height = h;}
 	
+	//helper method to allow for the easy printing of the game state in the proper format
 	public void printNode() {
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++)
@@ -467,6 +621,7 @@ class Node{
 		}
 	}
 	
+	//helper method to allow nodes to be easily compared
 	public boolean isDifferentState(Node n2) {
 		boolean ret = false;
 		for(int i = 0; i < 3; i++)
@@ -476,18 +631,3 @@ class Node{
 		return ret;
 	}
 }
-
-//search tree for shortest path
-
-
-
-
-
-
-
-
-
-
-
-
-
